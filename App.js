fcfa0d7c22e20b4f12b602ee037886802cf3e1a9
AdithyanValloor <165929678+AdithyanValloor@users.noreply.github.com>
2025-01-01 22:49:07 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+const express = require('express')
+
+const {productList} = require('./ProductList')
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+
+    res.status(200).send("Books API")
+
+})
+
+app.get('/products',(req, res) => {
+    
+    res.status(200).send(productList)
+    
+})
+
+app.get('/products/:id', (req, res) => {
+
+    try {
+        
+        const productId = parseInt(req.params.id)
+
+        const product = productList.find(prod => prod.id === productId)
+
+        if(!product){
+            res.status(404).json({message:"Product not found"})
+        }
+
+        res.status(200).send(product)
+
+        
+    } catch (error) {
+        res.status(404).json({error:error})
+    }
+
+})
+
+app.listen(port, ()=> console.log("Server started"))
---
+++ ProductList.js
@@ -0,0 +1,43 @@
+const productList = [
+    {
+        id:1,
+        name:"First You Write a Sentence",
+        author:"Joe Moran",
+        description:"An exploration of crafting clear and compelling sentences, offering insights into the art of writing.",
+        price:499
+    },
+
+    {
+        id:2,
+        name:"How to Write a Sentence: And How to Read One",
+        author:"Stanley Fish",
+        description:"A guide that delves into the structure and significance of sentences, enhancing both writing and reading skills. ",
+        price:899
+    },
+
+    {
+        id:3,
+        name:"The 5 Sentence Method: How to Write Your D*mn Book, Already",
+        author:"Rebecca Thorne",
+        description:"A straightforward approach to plotting and writing fiction, breaking down the process into manageable steps.",
+        price:649
+    },
+
+    {
+        id:4,
+        name:"Several Short Sentences About Writing",
+        author:"Verlyn Klinkenborg",
+        description:"Offers unconventional advice on writing, focusing on the craft of constructing effective sentences.",
+        price:789
+    },
+
+    {
+        id:5,
+        name:"On Writing: A Memoir of the Craft",
+        author:"Stephen King",
+        description:"Part memoir, part master class, this book shares the experiences, habits, and convictions that have shaped King's writing.",
+        price:999
+    }
+]
+
+module.exports = {productList}
